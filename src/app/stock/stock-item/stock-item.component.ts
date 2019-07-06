3b1b68df549cf7a03946cf9731ab9a2cabe8ee0d
LightStein <LightStein@users.noreply.github.com>
2019-07-07 00:26:23 +0400

    Create stock-item component
    Created stock-item component wit a basic (hardcoded) functinality. used Interpolation and one-way data binding

---
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-item',
+  templateUrl: './stock-item.component.html',
+  styleUrls: ['./stock-item.component.scss']
+})
+export class StockItemComponent implements OnInit {
+  public name: string;
+  public code: string;
+  public price: number;
+  public previousPrice: number;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.name = 'Test Stock Company';
+    this.code = 'TSC';
+    this.price = 85;
+    this.previousPrice = 80;
+  }
+
+}
