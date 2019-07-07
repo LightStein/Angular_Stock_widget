5b04d1c1ba670918b2b633ad18fc5d18d1637d42
LightStein <LightStein@users.noreply.github.com>
2019-07-08 01:59:04 +0400

    Optimize code
    Optimize code by Encapsulating stock class to model/stock

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -1,24 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Stock } from '../../model/stock';
+
 @Component({
   selector: 'app-stock-item',
   templateUrl: './stock-item.component.html',
   styleUrls: ['./stock-item.component.scss']
 })
 export class StockItemComponent implements OnInit {
-  public name: string;
-  public code: string;
-  public price: number;
-  public previousPrice: number;
-  public positiveChange: boolean;
+  public stock: Stock;
 
   constructor() { }
 
   ngOnInit() {
-    this.name = 'Test Stock Company';
-    this.code = 'TSC';
-    this.price = 85;
-    this.previousPrice = 80;
-    this.positiveChange = this.price >= this.previousPrice;
+    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+    // this.favoriteText = this.favorite ? 'Remove from Favorites' : 'Add to Favorites';
+  }
+  toggleFavorite(event) {
+    console.log('We are toggling the favorite for this stock', event);
+    this.stock.favorite = !this.stock.favorite;
   }
 }
